45a6ec4feffaa4fe56676ab6394ad3fe55710faa
sara526 <71785160+sara526@users.noreply.github.com>
2025-09-26 22:00:15 +0300

    fix problem

--- main.js
+++ main.js
@@ -1,79 +1,43 @@
-// let lightBoxContainer = document.querySelector('.light-box-container');
-// let lightBoxItem = document.querySelector('.light-box-item');
-// let imageItem = Array.from(document.querySelectorAll('.item img'));
-// let currentSlide = 0;
-// let prevBtn = document.getElementById('prevBtn'); 
-// let closeBtn = document.getElementById('closeBtn');
-// let nextBtn = document.getElementById('nextBtn');
-
-
-// for (let i = 0; i < imageItem.length; i++) {
-//     imageItem[i].addEventListener('click', function(e) {
-//         let imageSrc = e.target.getAttribute('src');
-//         currentSlide =imageItem.indexOf(e.target);
-//         console.log(currentSlide);
-//         lightBoxItem.style.backgroundImage = `url(${imageSrc})`;
-//         lightBoxContainer.classList.replace('d-none', 'd-flex');
-//     })
-// }
-//  function nextSlide() {
-//     currentSlide++;
-//     if (currentSlide == imageItem.length) {
-//         currentSlide = 0;
-//     }    
-//     let imageSrc = imageItem[currentSlide].getAttribute('src');
-//     lightBoxItem.style.backgroundImage = `url(${imageSrc})`;
-
-//  }
-
-//  function prevSlide(){
-//     currentSlide--;
-//     if (currentSlide < 0) {
-//         currentSlide = imageItem.length-1;
-//     }
-//     let imageSrc = imageItem[currentSlide].getAttribute('src');
-//     lightBoxItem.style.backgroundImage = `url(${imageSrc})`;
-//  }
-
-//  nextBtn.addEventListener('click',nextSlide);
-//  prevBtn.addEventListener('click',prevSlide);
-//  closeBtn.addEventListener('click', function(){
-//     lightBoxContainer.classList.replace('d-flex', 'd-none');
-//  });
-
-
 let lightBoxContainer = document.querySelector('.light-box-container');
 let lightBoxItem = document.querySelector('.light-box-item');
 let imageItem = Array.from(document.querySelectorAll('.item img'));
 let currentSlide = 0;
-
 let prevBtn = document.getElementById('prevBtn'); 
 let closeBtn = document.getElementById('closeBtn');
 let nextBtn = document.getElementById('nextBtn');
 
 for (let i = 0; i < imageItem.length; i++) {
-  imageItem[i].addEventListener('click', function(e) {
-    let imageSrc = e.target.getAttribute('src');
-    currentSlide = imageItem.indexOf(e.target);
+    imageItem[i].addEventListener('click', function(e) {
+        let imageSrc = e.target.getAttribute('src');
+        currentSlide =imageItem.indexOf(e.target);
+        console.log(currentSlide);
+        lightBoxItem.style.backgroundImage = `url(${imageSrc})`;
+        lightBoxContainer.classList.replace('d-none', 'd-flex');
+    })
+}
+ function nextSlide() {
+    currentSlide++;
+    if (currentSlide == imageItem.length) {
+        currentSlide = 0;
+    }    
+    let imageSrc = imageItem[currentSlide].getAttribute('src');
     lightBoxItem.style.backgroundImage = `url(${imageSrc})`;
-    lightBoxContainer.classList.replace('d-none', 'd-flex');
-  });
-}
+ }
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % imageItem.length;
-  let imageSrc = imageItem[currentSlide].getAttribute('src');
-  lightBoxItem.style.backgroundImage = `url(${imageSrc})`;
-}
+ function prevSlide(){
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = imageItem.length-1;
+    }
+    let imageSrc = imageItem[currentSlide].getAttribute('src');
+    lightBoxItem.style.backgroundImage = `url(${imageSrc})`;
+ }
 
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + imageItem.length) % imageItem.length;
-  let imageSrc = imageItem[currentSlide].getAttribute('src');
-  lightBoxItem.style.backgroundImage = `url(${imageSrc})`;
-}
+ nextBtn.addEventListener('click',nextSlide);
+ prevBtn.addEventListener('click',prevSlide);
+ closeBtn.addEventListener('click', function(){
+    lightBoxContainer.classList.replace('d-flex','d-none');
+ });
 
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);
-closeBtn.addEventListener('click', function() {
-  lightBoxContainer.classList.replace('d-flex', 'd-none');
-});
+
+
